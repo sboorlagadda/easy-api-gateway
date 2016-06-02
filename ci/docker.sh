7a3bd2f87d292e70c5fa7940e4b569cb17e29f09
Sai Boorlagadda <sboorlagadda@pivotal.io>
2016-06-02 08:58:24 -0700

    reverted the jar reference and added a task output

--- ci/docker.sh
+++ ci/docker.sh
@@ -3,4 +3,5 @@ export TERM=${TERM:-dumb}
 
 set -e
 
-cp apigateway-jar/apigateway*.jar easy-api-gateway/src/main/docker
+cp apigateway-jar/apigateway*.jar image
+cp easy-api-gateway/src/main/docker/Dockerfile image
